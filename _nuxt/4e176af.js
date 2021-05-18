e5114e63bc1ecfbdaaf773ae3b868e9e68fa447e
Ian Fan <ian@pollex.com.tw>
2021-05-18 09:19:28 +0800

    static

---
+++ _nuxt/4e176af.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{366:function(n,e,t){"use strict";t.r(e);var l={},c=t(45),component=Object(c.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  Demo Page\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
